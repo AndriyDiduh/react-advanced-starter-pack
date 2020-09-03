6b88bafaee337ed5a6db561f87ae8be21bc40b29
Andriy Diduh <damien_aron@yahoo.com>
2020-09-03 20:25:41 +0300

    Small change to App base.

--- src/app_generate.tsx
+++ src/app_generate.tsx
@@ -5,28 +5,14 @@ import Axios from 'axios'
 import 'Scss/all.scss'
 import 'Scss/fonts.scss'
 
-// GET NAVIGATION DATA from STATE - work Only ONCE
-//const navigation = new Navigation(false)
-//const activatedPage = navigation.state.activated_page
-
-export class AppGenerate extends React.Component<AppGenerateProps, AppGenerateState> {
-    public constructor(props: AppGenerateProps) {
+// Generate APP
+export default class AppGenerate extends React.Component<any, any> {
+    public constructor(props: any) {
         super(props)
         this.state = {}
     }
 
-    public componentDidMount = (): any => {}
-
-    public componentWillUnmount = (): any => {}
-
-    private onRemoteStateChange = (): void => {}
-
-
     public render = (): JSX.Element => {
-        return (
-            <div className="app">
-                React Advanced APP
-            </div>
-        )
+        return <div className="app">React Advanced APP</div>
     }
 }
